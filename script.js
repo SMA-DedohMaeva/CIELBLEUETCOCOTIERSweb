f6a3fdfc9f8e3614fd434327e1636076bd533a35
SMA <smagencyci@gmail.com>
2025-10-22 11:41:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const hamburger = document.getElementById('hamburger');
-const menu = document.getElementById('liste-menu');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('show');
+const hamburger = document.getElementById("hamburger");
+const menuTel = document.getElementById("menu-telephone");
+const croixFermer = document.getElementById("croix-fermer");
+
+hamburger.addEventListener("click", () => {
+  menuTel.classList.add("actif");
+  hamburger.style.display = "none"; // cacher hamburger
+});
+
+croixFermer.addEventListener("click", () => {
+  menuTel.classList.remove("actif");
+  hamburger.style.display = "flex"; // réafficher hamburger
 });
